026088ee9fb124decbb474e7fea3091981b30c00
royHridoy <royhridoy1428@gmail.com>
2023-07-21 23:11:18 +0600

    Initial Release

---
+++ README.md
@@ -0,0 +1,13 @@
+# Handling reactivity with vue.js 3
+
+# Preview
+
+
+# Icons
+<a target="_blank" href="https://icons8.com/icon/e9V-XJYOmiTY/light-on">Light On</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+
+<a target="_blank" href="https://icons8.com/icon/51368/light-off">Light Off</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+
+# Vue 3 + Vite
+
+This template should help get you started developing with Vue 3 in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { reactive } from 'vue';
+const lights = reactive([1, 0, 0])
+
+const lightImage = {
+    on: 'on.png',
+    off: 'off.png'
+}
+
+</script>
+
+<template>
+    <section class="container mx-auto flex items-center flex-col">
+        <h1 class="text-center text-2xl py-10">Switching Bulb</h1>
+        <p class="p-5">Click the switch to on off the bulb</p>
+        <div class="flex justify-between space-x-6">
+            <div class="p-10 border w-96 flex flex-col space-y-5 justify-between">
+
+                <button v-for="(light, index) in lights" :key="light" :class="1 == light ? 'bg-blue-500' : 'bg-gray-500'"
+                    @click="lights[index] = !lights[index]"
+                    class="hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    {{ light == 1 ? `Light ${index + 1} On` : `Light ${index + 1} Off` }}
+                </button>
+
+            </div>
+            <div class="border w-96 flex flex-col space-y-5 px-10 py-5">
+                <img v-for="light in lights" :key="light" class="w-20" :src="light ? lightImage.on : lightImage.off" alt="">
+            </div>
+        </div>
+    </section>
+</template>
+
+<style scoped>
+
+</style>
